d7c632e8af8d661a3d0534c4354a4dd278333933
Ole Christian Langfjæran <judoole@gmail.com>
2013-08-23 10:19:37 +0200

    Using same tests as JMeter Benchmark

--- locust/locustfile.py
+++ locust/locustfile.py
@@ -2,25 +2,29 @@ from locust import Locust, TaskSet, task
 
 class UserBehavior(TaskSet):
     def on_start(self):
-        """ on_start is called when a Locust start before any task is scheduled """
-        #self.login()
-
-    #def login(self):
-        #self.client.post("/login", {"username":"ellen_key", "password":"education"})
-
-    @task(2)
-    def helloworld(self):
-    	self.client.get("/examples/servlets/servlet/HelloWorldExample")
+        """ on_start is called when a Locust start before any task is scheduled """    
 
     @task(1)
-    def requestinfo(self):
-        self.client.get("/examples/servlets/servlet/RequestInfoExample")
+    def index(self):
+        self.client.get("/examples/servlets/index.html")
 
     @task(1)
+    def sessionExample1(self):
+        self.client.get("/examples/servlets/servlet/SessionExample")
+
+    @task(1)
+    def sessionExample2Post(self):
+        self.client.post("/examples/servlets/servlet/SessionExample", {"dataname" : "TOTO", "datavalue" : "TITI"})
+    
+    @task(1)
+    def sessionExample3(self):
+        self.client.get("/examples/servlets/servlet/SessionExample")
+
+    '''@task(1)
     def requestparams(self):
         with self.client.post("/examples/servlets/servlet/RequestParamExample", {"firstname":"Clark", "lastname":"Kent"}, catch_response=True) as response:
             if "Clark" not in response.content:
-                response.failure("Could not find firstname Clark in response")
+                response.failure("Could not find firstname Clark in response")'''
 
 class WebsiteUser(Locust):
     task_set = UserBehavior
