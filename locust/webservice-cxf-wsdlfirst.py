f39b33845f650e02b9280cb54aaa739b0009b1f9
Ole Christian Langfjæran <judoole@gmail.com>
2013-08-25 10:43:04 +0200

    Webservices. More of that.

--- locust/webservice-cxf-wsdlfirst.py
+++ locust/webservice-cxf-wsdlfirst.py
@@ -1,16 +1,26 @@
 from locust import Locust, TaskSet, task
 
-customersByName = open("getCustomersByName.xml", "rb")
+customersByName = open("getCustomersByName.xml", "r").read()
+updateCustomer = open("updateCustomer.xml", "r").read()
 
 class CxfWsdlFirst(TaskSet):
-    def on_start(self):
-        """ on_start is called when a Locust start before any task is scheduled """    
-
+    
+    # Update does nothing. We just check that the response is in the 200-numbers.
+    @task(1)
+    def update(self):
+        response = self.client.post(url="/wsdl_first/services/CustomerServicePort",
+        	data=updateCustomer,
+        	name="Update Customer")        
+    
+    #The Webservice returns a customer with id 0 and the name we provided (Clark Kent)
     @task(1)
     def getCustomer(self):
-        response = self.client.post(url="/wsdl_first/services/CustomerServicePort",
-        	data=customersByName)
-        print "Response content:", response.content
+        with self.client.post(url="/wsdl_first/services/CustomerServicePort",
+        	data=customersByName,
+        	name="Get Customer",
+        	catch_response=True) as response:
+        		if "Clark Kent" not in response.content:
+        			response.failure("Could not find firstname Clark in response")
 
 class WebsiteUser(Locust):
     task_set = CxfWsdlFirst
